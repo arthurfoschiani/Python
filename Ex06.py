bb7146c0d7829cccdb18a784c2f1fac1429c93f8
Arthur Foschiani <48352436+arthurfoschiani@users.noreply.github.com>
2022-08-26 21:11:12 -0300

    Delete Ex06.py

--- Ex06.py
+++
@@ -1,7 +0,0 @@
-#Conversor real para dolar
-
-cotacao = float(input("Digite a cotação do dólar: "))
-
-qntDinheiro = float(input("Digite a quantidade de dólar que deseja converter: "))
-
-print("O valor corresponde a R${}".format(qntDinheiro*cotacao))
