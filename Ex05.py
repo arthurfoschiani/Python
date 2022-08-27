c8c0d92f1e7cc836644d21168e9fd5b2e23cfefe
Arthur Foschiani <48352436+arthurfoschiani@users.noreply.github.com>
2022-08-26 21:11:06 -0300

    Delete Ex05.py

--- Ex05.py
+++
@@ -1,5 +0,0 @@
-#Converter temperatura de graus Celsius para Fahrenheit
-
-celsius = float(input("Digite a temperatura em Celsius: "))
-
-print("A temperatura em Fahrenheit é: {}".format(celsius*1.8+32))
